6321baa99e356e60ed4e3b581c43d8a63d93403d
unknown <MatheusAssis34>
2025-03-13 21:20:22 -0300

    projeto_controle_de_estoque

--- core/settings.py
+++ core/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/statics')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
