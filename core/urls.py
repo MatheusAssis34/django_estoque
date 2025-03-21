82047d42c33b1215c21213f06770eab6b1f1a0c9
unknown <MatheusAssis34>
2025-03-20 21:14:14 -0300

    update produtos

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,20 @@
+from pydoc import doc
 from django.contrib import admin
 from django.urls import path
-from apps.produtos.views import ProdutoListView, ProdutoCreateView
+from apps.produtos.views import ProdutoListView, ProdutoCreateView, ProdutoUpdateView
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', ProdutoListView.as_view(), name ='produtos_Lista'),
-    path('produto/add/', ProdutoCreateView.as_view(), name = 'produto_create'),
+    path('produto/add/', ProdutoCreateView.as_view(), name = 'produto-create'),
+    path('produto/update/<int:id>', ProdutoUpdateView.as_view(), name='produto-update')
     
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+
